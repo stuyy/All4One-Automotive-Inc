86ba5ecb845f50ca0f181f32c975545c52474416
Anson Foong <ansfoong@gmail.com>
2019-12-27 10:30:52 -0500

    Send 403 code if user enters incorrect credentials.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const User = require('../models/User');
 const passport = require('passport');
 
-router.post('/login', passport.authenticate('local'), async (req, res) => {
-    res.send(200);
+router.post('/login', passport.authenticate('local', { failureRedirect: 'unauthorized'}), async (req, res) => {
+    res.status(200).json({
+        status: 200
+    })
 });
 
+router.get('/unauthorized', (req, res) => res.send(403));
 router.get('/login', (req, res) => res.send(req.session));
 module.exports = router;
