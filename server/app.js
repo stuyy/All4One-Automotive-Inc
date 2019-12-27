366678600edd197ef40a6d472df7765d6180d092
Anson Foong <ansfoong@gmail.com>
2019-12-27 09:08:06 -0500

    added sessions and local strategy

--- server/app.js
+++ server/app.js
@@ -10,11 +10,27 @@ const db = require('./database/db')();
 db.on('error', () => console.log('connection error'));
 db.once('open', () => console.log('connected to DB.'))
 
+const passport = require('passport');
+const local = require('./strategies/local');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true, maxAge: 60000 * 60 * 24 * 365 },
+    store: new MongoStore({ mongooseConnection: db })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const careers = require('./routes/careers');
 const auth = require('./routes/auth');
