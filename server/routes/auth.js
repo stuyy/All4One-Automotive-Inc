847febbcee0a5f2a923e9fb0e93f79183ae5b627
Anson Foong <ansfoong@gmail.com>
2019-12-28 15:09:13 -0500

    Added logout route on backend server to destroy user session.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,4 +17,17 @@ router.get('/authenticated', (req, res) => {
     if(req.user) res.status(200).json({ status: 200, name: req.user.username });
     else res.status(403).json({ status: 403 });
 })
+
+router.get('/logout', (req, res) => {
+    if(req.user) {
+        console.log("Logging out");
+        req.logout();
+        res.status(200).json({
+            msg: "Logged out"
+        });
+    }
+    else {
+        res.send(403);
+    }
+})
 module.exports = router;
