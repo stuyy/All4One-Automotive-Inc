fb861f5015fd9cff117130a099021e9554806715
Anson Foong <ansfoong@gmail.com>
2019-12-27 20:35:28 -0500

    BackendService has postJobListing function will will invoke isAuthorized to ensure user is authed first.

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { mergeMap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,5 +14,15 @@ export class BackendService {
       responseType: 'json',
       withCredentials: true
     });
+  }
+  public isAuthorized() : Observable<any> {
+    return this.http.get<any>('http://localhost:3450/auth/authenticated', {
+      withCredentials: true
+    });
+  }
+
+  public postJobListing(job) : Observable<any> {
+    return this.isAuthorized().pipe(
+      mergeMap(v => this.http.post('http://localhost:3450/jobs/create', job, { withCredentials: true })));
   }
 }
