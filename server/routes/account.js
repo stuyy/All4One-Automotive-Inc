16a9b9b538092427f7a8f7fb12a336543a027dff
Anson Foong <ansfoong@gmail.com>
2019-12-29 13:46:21 -0500

    added password update route on server

--- server/routes/account.js
+++ server/routes/account.js
@@ -30,14 +30,19 @@ async (req, res) => {
         if(req.user && req.user.type === 'admin') {
             delete req.body.confirm;;
             try {
-                let newUser = new User(req.body)
+                let newUser = new User(req.body);
+                newUser.createdBy = req.user.username;
                 await newUser.hashPassword();
                 await newUser.save(); // Save User.
-                res.sendStatus(201);
+                res.status(201).json({
+                    status: "Success"
+                })
             }
             catch(err) {
                 console.log(err);
-                res.send(403);
+                res.status(403).json({
+                    error: "Error."
+                })
             }
         }
         else {
@@ -46,4 +51,32 @@ async (req, res) => {
     }
 })
 
+router.put('/password/update', [ 
+    check('password').isLength({ min: 5 }),
+    body('password').custom((value, { req }) => {
+    if(value !== req.body.confirm) throw new Error("Passwords do not match");
+    return true;
+})], 
+async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        res.status(422).json({ msg: 'Unable to process inputed data' });
+    }
+    else {
+        if(req.user) {
+            console.log("Updating password for ", req.user.username);
+            res.status(200).json({ msg: "Password Updated"});
+            try {
+                let findUser = await User.findById(req.user.id);
+                console.log(findUser);
+                let update = await findUser.updatePassword(req.body.password);
+                console.log(update);
+            }
+            catch(err) {
+                console.log(err);
+            }
+
+        }
+    }
+})
 module.exports = router;
