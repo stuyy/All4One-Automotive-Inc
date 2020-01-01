abdca75aca23a6a5db9bcd7846ca04eb8fc0b2e8
Anson Foong <ansfoong@gmail.com>
2020-01-01 18:56:47 -0500

    Set urlencoded to true to accept any type

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'))
 app.use(session({
