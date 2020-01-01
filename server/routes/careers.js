aca58f1db72a0be19cd8b14c90bcf2912b490d23
Anson Foong <ansfoong@gmail.com>
2020-01-01 18:57:04 -0500

    Imported Multer Module to handle file inputs

--- server/routes/careers.js
+++ server/routes/careers.js
@@ -1,14 +1,11 @@
 const router = require('express').Router();
 const { check, validationResult } = require('express-validator');
+const JobApplicantion = require('../models/JobApplication');
+const multer = require('multer')({ dest: 'C:\\Users\\Anson\\Desktop\\JobResumes'});
 
-router.post('/apply', (req, res) => {
+router.post('/apply', multer.single('resume'), (req, res) => {
     console.log(req.body);
+    console.log(req.file)
 });
 
-/**
- * Create Job Listings
- */
-router.post('/create', (req, res) => {
-    
-});
 module.exports = router;
