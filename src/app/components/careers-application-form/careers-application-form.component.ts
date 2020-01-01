f62b7ebd7376fe62cbbe4531ae2943f56fa53ebc
Anson Foong <ansfoong@gmail.com>
2020-01-01 18:57:40 -0500

    Pass a FormData object with all fields appended.

--- src/app/components/careers-application-form/careers-application-form.component.ts
+++ src/app/components/careers-application-form/careers-application-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { JobService } from 'src/app/services/job.service';
+import JobApplication from 'src/app/models/JobApplication';
+import { FileUploadControl, FileUploadValidators } from '@iplab/ngx-file-upload';
 
 @Component({
   selector: 'app-careers-application-form',
@@ -18,7 +21,10 @@ export class CareersApplicationFormComponent implements OnInit {
   phoneNumber: FormGroup;
   attachments: FormGroup;
   comments: FormGroup;
-  constructor(private _formBuilder: FormBuilder) {}
+  //files = new FileUploadControl(FileUploadValidators.filesLimit(1));
+  //uploaded: Array<File> = [];
+
+  constructor(private _formBuilder: FormBuilder, private jobService: JobService) {}
 
   ngOnInit() {
     this.fullName = this._formBuilder.group({
@@ -37,16 +43,26 @@ export class CareersApplicationFormComponent implements OnInit {
     this.comments = this._formBuilder.group({
       comments: ['', Validators.required]
     })
+
   }
   apply() {
-    console.log(this.jobId)
+    let formData = new FormData();
+    formData.append('firstName', this.fullName.value.firstName);
+    formData.append('lastName', this.fullName.value.lastName);
+    formData.append('email', this.email.value.email);
+    formData.append('phoneNumber', this.phoneNumber.value.phoneNumber);
+    formData.append('comments', this.comments.value.comments);
+    formData.append('resume', this.attachments.value.attachments._files[0], this.attachments.value.attachments._files[0].name);
+    this.jobService.applyJob(formData)
+      .subscribe(res => console.log(res),
+      err => console.log(err));
   }
   getEmailError() {
     return this.email.get('email').errors.required ? 'Please enter an e-mail address' : this.email.get('email').errors.email ? 'Please enter a valid email' : '';
   }
   validatePhoneNumber(phone: FormGroup) {
     let phoneNumber : string = phone.value;
-    let regex = new RegExp(/([0-9]{3}-){2}[0-9]{4}/);
+    let regex = new RegExp(/^([0-9]{3}-){2}[0-9]{4}$/);
     console.log(phoneNumber.match(regex))
     return regex.test(phoneNumber) ? null : { invalidPhoneNumber: true }
   }
