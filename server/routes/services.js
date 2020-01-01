8efe66d2cc250890e2302bb30fd737bc8188f5d2
Anson Foong <ansfoong@gmail.com>
2020-01-01 09:43:58 -0500

    Added services route to allow users to request a service. Sends mail using Nodemailer.

---
+++ server/routes/services.js
@@ -0,0 +1,97 @@
+
+const router = module.exports = require('express').Router();
+const ServiceRequest = require('../models/ServiceRequest');
+const MailTransporter = require('../utils/MailTransporter');
+
+router.get('/', (req, res) => {
+    res.send(200);
+}); 
+
+router.post('/', async (req, res) => {
+    let { name, phoneNumber, email, type } = req.body;
+    let transport = new MailTransporter();
+    let htmlTemplate = getEmailTemplate(name, phoneNumber, email, type);
+    try {
+        let mail = await transport.sendMail({
+            receiver: process.env.GMAIL_USER,
+            sender: process.env.EMAIL_USER,
+            subject: `Service Request from ${name}`,
+            html: htmlTemplate
+        });
+        let service = new ServiceRequest({
+            serviceType: type,
+            name,
+            email,
+            phoneNumber
+        });
+        await service.save();
+        res.status(201).json({success: true, msg: "Success", data: service });
+    }
+    catch(err) {
+        res.status(500).json({ error: 500, msg: "Something went wrong, please try again."})
+    }
+});
+
+function getEmailTemplate(name, phoneNumber, email, type) {
+    return `
+    <html>
+        <head>
+            <style>
+                * {
+                    padding: 0;
+                    margin: 0;
+                }
+                header {
+                    width: 100%;
+                    height: 150px;
+                    background-color: #212121;
+                    display: flex;
+                    justify-content: space-between !important;
+                    color: #DB162F;
+                }
+                .logo {
+                    width: 350px;
+                }
+                .email-message-body {
+                    background-color: #DB162F;
+                    color: white;
+                    padding: 50px;
+                    font-size: 1.5em;
+                }
+                footer {
+                    text-align: center;
+                    background-color: #212121;
+                    height: 70px;
+                    width: 100%;
+                    padding: 10px;
+                    color: #DB162F;
+                    font-size: 1.5em;
+                }
+            </style>
+        </head>
+        <body>
+            <header>
+                <div>
+                    <img class="logo" src="https://www.all4oneautomotiveinc.com/assets/logo.png">
+                </div>
+                <div>
+                    <h1>Service Request - ${type}</h1>
+                </div>
+            </header>
+            <section class="email-message-body">
+                <h2>You recevied a service request</h2>
+                <p>Hey Danish! You received a new service request</p>
+                <h3>Details</h3>
+                <h4>Service Requested: ${type}</h4>
+                <h4>Name: ${name}</h4>
+                <h4>Email: ${email}</h4>
+                <h4>Phone Number: <a href="tel:${phoneNumber}">${phoneNumber}</a></h4>
+            </section>
+            <footer>
+                <h1>All 4 One Automotive Inc. 2019</h1>
+            </footer>
+        </body>
+    </html>
+
+    `
+}
