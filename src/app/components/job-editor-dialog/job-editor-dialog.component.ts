5afeb72db114e77c4e9e079376291af56249f35b
Anson Foong <ansfoong@gmail.com>
2020-01-01 15:42:27 -0500

    Added submitEditJob function, it will call the jobService to make a PUT request and then on success it will emit an event

--- src/app/components/job-editor-dialog/job-editor-dialog.component.ts
+++ src/app/components/job-editor-dialog/job-editor-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Inject, AfterViewInit } from '@angular/core';
 import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { JobListing } from 'src/app/models/JobListing';
+import { JobService } from 'src/app/services/job.service';
 
 @Component({
   selector: 'app-job-editor-dialog',
@@ -10,26 +12,34 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 export class JobEditorDialogComponent implements OnInit {
 
   jobDescription: FormControl;
-  jobTemplate: string;
-  jobTitle: string;
+  jobListing: JobListing;
   jobForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<JobEditorDialogComponent>,
+    private jobService: JobService,
     @Inject(MAT_DIALOG_DATA) public data: any) { 
-      this.jobTemplate = this.data.template;
-      this.jobTitle = this.data.jobTitle;
+      this.jobListing = this.data.jobListing;
       this.jobForm = this.formBuilder.group({
-        jobTitle: new FormControl(this.jobTitle, [Validators.required]),
-        jobDescription: new FormControl(this.jobTemplate, [Validators.required])
+        jobTitle: new FormControl(this.jobListing.jobTitle, [Validators.required]),
+        jobDescription: new FormControl(this.jobListing.jobDescription, [Validators.required])
       })
     //this.jobDescription = new FormControl(this.jobTemplate, [Validators.required]);
-
   }
 
   ngOnInit() {
    
   }
-
+  submitEditJob() {
+    this.jobService.editJob(this.jobListing._id, this.jobForm.value)
+      .subscribe((res : any) => {
+        console.log(res)
+        this.jobService.getJobEvents().emit({
+          name: 'jobEdit',
+          oldJob: this.jobListing,
+          newJob: res.job
+        });
+      }, err => console.log(err));
+  }
 
 }
