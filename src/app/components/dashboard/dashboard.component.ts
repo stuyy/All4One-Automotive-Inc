3eda9ee233c664400c9405ba445fa028c3223961
Anson Foong <ansfoong@gmail.com>
2020-01-01 15:44:19 -0500

    Added dialog, snackbar, and jobService to be interacted with when a user completes an action.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from 'src/app/services/backend.service';
 import { Router } from '@angular/router';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatSnackBar } from '@angular/material';
 import { DialogOverviewComponent } from '../dialog-overview/dialog-overview.component';
 import { JobListingCreatorComponent } from '../job-listing-creator/job-listing-creator.component';
+import { JobService } from 'src/app/services/job.service';
 
 
 @Component({
@@ -15,8 +16,16 @@ export class DashboardComponent implements OnInit {
 
   constructor(private backendService : BackendService,
     private router: Router,
-    private dialog: MatDialog) {
-    
+    private dialog: MatDialog,
+    private snackbar: MatSnackBar,
+    private jobService: JobService) {
+      this.jobService.getJobEvents().subscribe(event => {
+        if(event.name === 'jobSubmit')
+        {
+          this.dialog.closeAll();
+          this.snackbar.open('Job successfully posted!')
+        }
+      });
   }
   ngOnInit() {
    
