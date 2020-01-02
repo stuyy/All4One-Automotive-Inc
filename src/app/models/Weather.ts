f19f0e3d697117dac57704e2f19de43764d49f59
Anson Foong <ansfoong@gmail.com>
2020-01-02 17:26:25 -0500

    Added Weather.ts interface to represent a Weather model.

---
+++ src/app/models/Weather.ts
@@ -0,0 +1,16 @@
+export default interface Weather {
+    coords: Object;
+    weather: Array<Object>;
+    base: string;
+    main: Object;
+    visibility: number;
+    wind: Object;
+    rain: Object;
+    clouds: Object;
+    dt: number;
+    sys: Object;
+    timezone: number;
+    id: number;
+    name: string;
+    cod: number;
+}
