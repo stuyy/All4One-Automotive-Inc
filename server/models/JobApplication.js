693d33664269600c2c618b506921859ef56cbb32
Anson Foong <ansfoong@gmail.com>
2020-01-02 10:21:55 -0500

    Added questionResponses field for jobapplicant schema

--- server/models/JobApplication.js
+++ server/models/JobApplication.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const jobApplicantSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
@@ -7,6 +6,20 @@ const jobApplicantSchema = new mongoose.Schema({
     phoneNumber: { type: String, required: true },
     resume: { type: String, required: true },
     comments: { type: String, required: true },
+    questionResponses: {
+        questionOne: {
+            question: {  type: String, required: true },
+            response: { type: String, required: true }
+        },
+        questionTwo: {
+            question: { type: String, required: true },
+            response: { type: String, required: true}
+        },
+        questionThree: {
+            question: { type: String, required: true },
+            response: { type: String, required: true}
+        },
+    },
     jobId: { type: String, required: true }
 }, { timestamps: true })
 
