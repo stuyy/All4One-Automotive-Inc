33dd6f687197115755dd68f24734757380d73d8a
Anson Foong <ansfoong@gmail.com>
2020-01-02 17:36:50 -0500

    Saved the city name to local storage

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WeatherService } from 'src/app/services/Weather/weather.service';
 import Weather from 'src/app/models/Weather';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
@@ -19,33 +19,43 @@ export class WeatherComponent implements OnInit {
     this.weatherInput = new FormControl('');
   }
   ngOnInit() {
-    navigator.geolocation.getCurrentPosition(pos => {
-      this.latitude = pos.coords.latitude;
-      this.longitude = pos.coords.longitude;
-      this.weatherService.getWeatherByCoords({ lat: this.latitude, lon: this.longitude})
-        .subscribe((data : Weather) => {
-          this.weather = data;
-          console.log(this.weather)
-          this.loading = false;
-        }, err => {
-          console.log(err);
-          this.loading = false;
-        })
-    }, err => {
-      // Make user enter location manually.
-      console.log(err);
-      this.blocked = true;
-      this.loading = false;
-    });
+    // Check Local Storage to see if city exists.
+    if(localStorage.getItem('city')) {
+      this.getWeather(null, localStorage.getItem('city'));
+      console.log("Local Storage")
+    }
+    else {
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.latitude = pos.coords.latitude;
+        this.longitude = pos.coords.longitude;
+        this.weatherService.getWeatherByCoords({ lat: this.latitude, lon: this.longitude})
+          .subscribe((data : Weather) => {
+            this.weather = data;
+            console.log(this.weather)
+            this.loading = false;
+          }, err => {
+            console.log(err);
+            this.loading = false;
+          })
+      }, err => {
+        // Make user enter location manually.
+        console.log(err);
+        this.blocked = true;
+        this.loading = false;
+      });
+    }
   }
-  getWeather() {
+  getWeather(event, city) {
+    if(event) {
+      city = this.weatherInput.value;
+      localStorage.setItem('city', city);
+    }
     this.loading = true;
-    this.weatherService.getWeatherByCity(this.weatherInput.value)
+    this.weatherService.getWeatherByCity(city)
       .subscribe((data : Weather) => {
         this.weather = data;
         this.blocked = false;
         this.loading = false;
       }, err => console.log(err));
   }
-
 }
