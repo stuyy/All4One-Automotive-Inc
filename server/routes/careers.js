6ca60b18456e3dcb616bbd830b96473e7ef6c0f3
Anson Foong <ansfoong@gmail.com>
2020-01-02 07:45:08 -0500

    Added multer storage to save files to destination. Added validation for job application on server side. Increment JobListing's applicants count by 1.

--- server/routes/careers.js
+++ server/routes/careers.js
@@ -1,11 +1,53 @@
 const router = require('express').Router();
 const { check, validationResult } = require('express-validator');
-const JobApplicantion = require('../models/JobApplication');
-const multer = require('multer')({ dest: 'C:\\Users\\Anson\\Desktop\\JobResumes'});
+const JobApplication = require('../models/JobApplication');
+const JobListing = require('../models/JobListing');
 
-router.post('/apply', multer.single('resume'), (req, res) => {
-    console.log(req.body);
-    console.log(req.file)
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'C:\\Users\\Anson\\Desktop\\JobResumes')
+    },
+    filename: (req, file, cb) => {
+        console.log("File:", file);
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({ storage: storage });
+
+router.post('/apply', upload.single('resume'), [
+    check('firstName').notEmpty(),
+    check('lastName').notEmpty(),
+    check('email').isEmail(),
+    check('comments').notEmpty(),
+    check('phoneNumber').custom((value, { req }) => {
+        let r = new RegExp(/^([0-9]{3}-){2}[0-9]{4}$/);
+        if(r.test(value)) 
+            return true;
+        else throw new Error("Invalid Phone Number")
+    }),
+], async (req, res) => {
+    let errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        console.log(errors)
+        res.status(422).json({ status: 422, error: 'Unable to process entity'})
+    }
+    else {
+        let { firstName, lastName, email, phoneNumber, comments, jobId } = req.body;
+        let resume = req.file.path;
+        let jobApplication = new JobApplication({ firstName, lastName, email, phoneNumber, resume, comments, jobId });
+        try {
+            await jobApplication.save();
+            console.log("Saved Job Application");
+            let foundJobListing = await JobListing.findById(jobId);
+            foundJobListing.applied();
+            res.status(201).json({ status: 201, message: "Job application received!"})
+        }
+        catch(err) {
+            console.log(err);
+            res.status(500).json({ status: 500, message: "An unknown error occured. Please try again later" });
+        }
+    }
 });
 
 module.exports = router;
