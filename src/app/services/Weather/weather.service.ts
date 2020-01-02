2425244ab8c2d060d9e133edd1905b15cd72f1b5
Anson Foong <ansfoong@gmail.com>
2020-01-02 17:26:36 -0500

    Added weather service used to fetch API for weather data

---
+++ src/app/services/Weather/weather.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import Weather from 'src/app/models/Weather';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { 
+    
+  }
+  getWeatherByCoords(coords) : Observable<Weather> {
+    return this.http.get<Weather>(`${environment.host}/weather/coordinates/${coords.lat}/${coords.lon}`);
+  }
+  getWeatherByCity(city) : Observable<Weather> {
+    return this.http.get<Weather>(`${environment.host}/weather/city/${city}`);
+  }
+}
