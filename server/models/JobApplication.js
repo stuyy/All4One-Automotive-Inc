fd9816358bfa6cb2922a8312c9f74f059390ba5f
Anson Foong <ansfoong@gmail.com>
2020-01-02 07:45:43 -0500

    Changed resume type from Buffer to String

--- server/models/JobApplication.js
+++ server/models/JobApplication.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
-const jobApplicantSchema = mongoose.Schema({
+const jobApplicantSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true }, 
     phoneNumber: { type: String, required: true },
-    attachments: { type: Buffer, required: true },
+    resume: { type: String, required: true },
     comments: { type: String, required: true },
     jobId: { type: String, required: true }
 }, { timestamps: true })
+
 
 const JobApplication = module.exports = mongoose.model('JobApplicants', jobApplicantSchema);
