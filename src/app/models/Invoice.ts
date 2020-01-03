c5a27bd12a4f03ba15269fbbc8ba8753c63c54de
Anson Foong <ansfoong@gmail.com>
2020-01-03 11:16:04 -0500

    Created Invoice model

---
+++ src/app/models/Invoice.ts
@@ -0,0 +1,9 @@
+export default interface Invoice {
+    id: string;
+    companyName: string;
+    checkId: string;
+    make: string;
+    model: string;
+    year: number;
+    description: string;
+}
