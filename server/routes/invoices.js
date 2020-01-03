b52d41caebe4c1031ebe3e118fd03d3e11c5e086
Anson Foong <ansfoong@gmail.com>
2020-01-03 11:47:31 -0500

    Added create route, takes body input and creates an Invoice object, then saves to database.

--- server/routes/invoices.js
+++ server/routes/invoices.js
@@ -1,4 +1,5 @@
 const router = module.exports = require('express').Router();
+const Invoice = require('../models/Invoice');
 
 function verify(req, res, next) {
     if(req.user && req.user.type === 'admin') {
@@ -14,6 +15,16 @@ router.get('/:id', (req, res) => {
 
 });
 
-router.post('/create', verify, (req, res) => {
-    console.log(req.body);
+router.post('/create', verify, async (req, res) => {
+    let invoice = { invoiceId, companyName, checkId, make, model, year, description } = req.body;
+    let newInvoice = new Invoice(invoice);
+    try {
+        let saveInvoice = await newInvoice.save();
+        console.log("Saved Invoice.", saveInvoice);
+        res.status(201).json({ status: 201, message: "Invoice Created.", invoice: saveInvoice })
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({ status: 500, message: "Error."})
+    }
 });
