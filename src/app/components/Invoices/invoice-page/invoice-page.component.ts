8d725f2a7606719159625ee587b07014cf15c950
Anson Foong <ansfoong@gmail.com>
2020-01-03 11:48:11 -0500

    Added InvoicePage and InvoiceItem components for rendering invoices.

---
+++ src/app/components/Invoices/invoice-page/invoice-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-page',
+  templateUrl: './invoice-page.component.html',
+  styleUrls: ['./invoice-page.component.css']
+})
+export class InvoicePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
