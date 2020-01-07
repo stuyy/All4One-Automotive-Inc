ff895461be3c808842cd4d936eecb62ee528fecb
Anson Foong <ansfoong@gmail.com>
2020-01-07 12:40:35 -0500

    Added getProfit and getProfits function

--- src/app/components/Profits/profits-service/profits.service.ts
+++ src/app/components/Profits/profits-service/profits.service.ts
@@ -12,6 +12,12 @@ export class ProfitsService {
   constructor(private http: HttpClient) { }
 
   postProfits(profit: Profit) : Observable<Profit> {
-    return this.http.post<Profit>(`${environment.host}/profits`, profit, { withCredentials: true });
+    return this.http.post<Profit>(`${environment.host}/profits/create`, profit, { withCredentials: true });
+  }
+  getProfit(id) : Observable<Profit> {
+    return this.http.get<Profit>(`${environment.host}/profits/${id}`, { withCredentials: true });
+  }
+  getProfits() : Observable<Array<Profit>> {
+    return this.http.get<Array<Profit>>(`${environment.host}/profits`, { withCredentials: true });
   }
 }
